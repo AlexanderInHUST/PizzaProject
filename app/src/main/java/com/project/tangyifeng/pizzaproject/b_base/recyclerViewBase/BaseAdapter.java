0efbcdb26fceee2959a6e72c4ccc667ca24e5445
yifeng tang <yifengtang_hust@outlook.com>
2017-05-23 09:53:51 +0800

    Second push for fetch-testing.

---
+++ app/src/main/java/com/project/tangyifeng/pizzaproject/b_base/recyclerViewBase/BaseAdapter.java
@@ -0,0 +1,13 @@
+package com.project.tangyifeng.pizzaproject.b_base.recyclerViewBase;
+
+/**
+ * Author: Alexander
+ * Email: yifengtang@hustunique.com
+ * Since: 2017/5/23.
+ */
+
+public class BaseAdapter {
+
+
+
+}
---
+++ app/src/main/java/com/project/tangyifeng/pizzaproject/b_base/recyclerViewBase/BaseListActivity.java
@@ -0,0 +1,10 @@
+package com.project.tangyifeng.pizzaproject.b_base.recyclerViewBase;
+
+/**
+ * Author: Alexander
+ * Email: yifengtang@hustunique.com
+ * Since: 2017/5/23.
+ */
+
+public class BaseListActivity {
+}
