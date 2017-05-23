315c6e56fcad32df457d41766cbcb7563e2c53b6
viseator <viseator@gmail.com>
2017-05-23 11:11:18 +0800

    add tabbar and style

--- app/src/main/java/com/project/tangyifeng/pizzaproject/f_mainPage/MainActivity.java
+++ app/src/main/java/com/project/tangyifeng/pizzaproject/f_mainPage/MainActivity.java
@@ -4,12 +4,34 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.project.tangyifeng.pizzaproject.R;
+import com.project.tangyifeng.pizzaproject.b_base.mvpBase.BaseActivity;
+import com.project.tangyifeng.pizzaproject.b_base.mvpBase.BasePresenter;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    protected BasePresenter getPresenter() {
+        return null;
+    }
+
+    @Override
+    protected int getLayout() {
+        return R.layout.tabbar_default;
+    }
+
+    @Override
+    protected boolean isToolbarEnable() {
+        return false;
+    }
+
+    @Override
+    protected void initEventAndData() {
+
     }
 }
